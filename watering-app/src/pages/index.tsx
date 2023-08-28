babab2566183be625d76e4255e8e66550736b91f
HaomingY <haoming.isaac@gmail.com>
2023-08-28 15:53:44 +0200

    first commit and set up the project

---
+++ watering-app/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+import React from "react";
+import SearchBar from "@/components/SearchBar";
+
+export default function Home() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
